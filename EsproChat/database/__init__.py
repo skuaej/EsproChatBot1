a21020e7eb09f37e45fedc91a5f5ddca4b38948c
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-06-22 16:20:46 +0530

    Update __init__.py

--- EsproChat/database/__init__.py
+++ EsproChat/database/__init__.py
@@ -1,4 +1,10 @@
+from pymongo import MongoClient
+
 import config
+
+Ritikdb = MongoClient(config.MONGO_URL)
+Ritik = Ritikdb["RitikDb"]["Ritik"]
+
+
 from .chats import *
 from .users import *
-from .clonestats import *

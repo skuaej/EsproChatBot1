aab1196191afe78ebcc3e59381eaef9c9a25465e
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-08-17 14:04:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "21553328"
+API_ID = "24411134"
 # -------------------------------------------------------------
-API_HASH = "3bcc137f124bc8518c30d461986e247b"
+API_HASH = "da78963da6eaaf521133e00628434271"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7940019678:AAFnZD4f0dM_PPZm9vbmuv_HGPxw6EjGswo")
+BOT_TOKEN = getenv("BOT_TOKEN", "8410601684:AAHFIJ25M0QNLcpjuOdcDyxvmYnpJDteXNM")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://oliva:oliva123@cluster0.6cohqfv.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = int(getenv("OWNER_ID", "6693611573"))
+OWNER_ID = int(getenv("OWNER_ID", "7850114307"))
 SUPPORT_GRP = "EsproSupport"
 UPDATE_CHNL = "EsproUpdate"
 OWNER_USERNAME = "Ur_Haiwan"
